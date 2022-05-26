ca5c2b30f3988c73239c24b4c727b1881d784a83
NormanFrieman <ezequiel-d.s@hotmail.com>
2022-05-26 15:47:43 -0300

    Initial commit

---
+++ src/main/java/build-gitactions/App.java
@@ -0,0 +1,13 @@
+package build-gitactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
